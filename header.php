e3cf430a61e24f0224ba0ed9f5baefb191a4272b
Isaac Sutherland <isaac@li1964-177.members.linode.com>
2019-08-13 17:58:22 +0000

    styling for menus

--- header.php
+++ header.php
@@ -19,12 +19,33 @@ $logo = "$base/i/far-logo.png";
     <span>Ottawa - Contact Us</span>
 </a>
 
+<script>
+function hasClass(el, cls) { return !!el.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)')); }
+function addClass(el, cls) { if (!hasClass(el, cls)) el.className += ' ' + cls; }
+function removeClass(el, cls) { if (hasClass(el, cls)) { el.className = el.className.replace(new RegExp('(^' + cls + '(\\s|$))|(\\s' + cls + ')'), ''); } }
+function toggleClass(el, cls) { if (hasClass(el, cls)) removeClass(el, cls); else addClass(el, cls); }
+</script>
 <div class="navtop">
 	<b class="fill"></b>
-	<span>Ottawa</span>
-	<a href="tel:1-877-851-9099">1-877-851-9099</a>
+    <a class="ham" href="#">&#9776;</a>
+    <a class="link oneline" href="/">Home</a>
+    <a class="link" href="/simplified-life">No Medical<br/> Life Insurance</a>
+    <a class="link" href="/critical-illness">Critical Illness<br/> Insurance</a>
+    <a class="link" href="/health-and-dental">Health &amp; Dental<br/> Insurance</a>
+    <a class="link oneline" href="/travel">Travel Insurance</a>
+    <a class="link oneline" href="/about-us">About Us</a>
+	<a class="tel" href="tel:1-613-408-7002">1-613-408-7002</a>
 </div>
-<br/>
+<script>
+var ham = document.querySelector('.ham'),
+    navtop = document.querySelector('.navtop');
+ham.addEventListener('click', function(e) {
+    addClass(navtop, 'animateHeight');
+    toggleClass(navtop, 'expand');
+    setTimeout(() => { removeClass(navtop, 'animateHeight'); }, 250);
+    e.preventDefault();
+});
+</script>
 <?php
 echo do_shortcode('[lqp_life_form]');
 ?>
