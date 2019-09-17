f4bef7e991c06b857071b6ea5d16981fe54a7188
Isaac Sutherland <isaac@li1964-177.members.linode.com>
2019-09-17 14:57:49 +0000

    latest styles and scripts

--- src/routes/common.js
+++ src/routes/common.js
@@ -2,6 +2,18 @@ export default {
   init() {
     // JavaScript to be fired on all pages
     console.log('common');
+    var tt = document.querySelectorAll('.tab');
+    var pp = document.querySelectorAll('div.page');
+    for (var i=0; i<tt.length; i++) {
+        tt[i].addEventListener('click', function(i,t) {
+            return function(e) {
+                for (var j=0; j<tt.length; j++) {
+                    tt[j].className = i==j ? 'tab sel' : 'tab';
+                    pp[j].className = i==j ? 'page sel' : 'page';
+                }
+            }
+        }(i, tt[i]));
+    }
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
